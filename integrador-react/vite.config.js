39865de28365169b9fabefbfe9f6281a4e0c13a6
patriciaCastillo <patrycast81@gmail.com>
2026-02-28 14:03:39 -0300

    configuracion

--- integrador-react/vite.config.js
+++ integrador-react/vite.config.js
@@ -4,15 +4,15 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-    server: {
-    proxy: {
-      "/api": {
-        target: "https://nucbaz-api.vercel.app",
-        changeOrigin: true,
-        secure: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
-    },
-  },
+  //   server: {
+  //   proxy: {
+  //     "/api": {
+  //       target: "https://nucbaz-api.vercel.app",
+  //       changeOrigin: true,
+  //       secure: true,
+  //       rewrite: (path) => path.replace(/^\/api/, ""),
+  //     },
+  //   },
+  // },
   
 })
